4ba58655b0f8d75e93d23f13838bc3d5de9b629b
Huu Ha <ha.nh@kynaforkids.vn>
2020-02-21 10:18:09 +0700

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,89 +1,111 @@
 'use strict';
-module.exports = function(grunt) {
+const sass = require('node-sass');
+module.exports = function (grunt) {
 
-  grunt.initConfig({
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: [
-        'Gruntfile.js',
-        'assets/js/**/*.js',
-        '!assets/build/app.min.js'
-      ]
-    },
-    sass: {
-      dist: {
-        options: {
-          style: 'compressed',
-          compass: false,
-          sourcemap: false
+    grunt.initConfig({
+        notify: {
+            sass: {
+                options: {
+                    title  : '<%= pkg.name %>',
+                    message: 'Sass Compiler finished'
+                }
+            },
+            js  : {
+                options: {
+                    title  : '<%= pkg.name %>',
+                    message: 'JS Compiler finished'
+                }
+            },
+            concat  : {
+                options: {
+                    title  : '<%= pkg.name %>',
+                    message: 'CONCAT finished'
+                }
+            }
         },
-        files: {
-          'assets/build/app.min.css': [
-              'assets/sass/app.scss'
-          ]
-        }
-      }
-    },
-    uglify: {
-      dist: {
-        files: {
-          'assets/build/app.min.js': [
-            'assets/js/app.js'
-          ]
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                'assets/js/**/*.js',
+                '!assets/build/app.min.js'
+            ]
         },
-        options: {
-          sourceMap: 'assets/build/app.min.js.map',
-          sourceMappingURL: '/assets/build/app.min.js.map'
+        sass: {
+            dist: {
+                options: {
+                    style: 'compressed',
+                    compass: false,
+                    sourcemap: false
+                },
+                files: {
+                    'assets/build/app.min.css': [
+                        'assets/sass/app.scss'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            dist: {
+                files: {
+                    'assets/build/app.min.js': [
+                        'assets/js/app.js'
+                    ]
+                },
+                options: {
+                    sourceMap: 'assets/build/app.min.js.map',
+                    sourceMappingURL: '/assets/build/app.min.js.map'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true//Default port 35729
+            },
+            sass: {
+                files: [
+                    'assets/sass/**/*.scss'
+                ],
+                tasks: ['sass']
+            },
+            js: {
+                files: [
+                    'assets/js/**/*.js'
+                ],
+                tasks: ['jshint', 'uglify']
+            },
+            html: {
+                files: [
+                    '**/*.html'
+                ]
+            }
+        },
+        clean: {
+            dist: [
+                'assets/build/app.min.css',
+                'assets/build/app.min.js'
+            ]
         }
-      }
-    },
-    watch: {
-      options: {
-        livereload: true
-      },
-      sass: {
-        files: [
-          'assets/sass/**/*.scss'
-        ],
-        tasks: ['sass']
-      },
-      js: {
-        files: [
-          'assets/js/**/*.js'
-        ],
-        tasks: ['jshint', 'uglify']
-      },
-      html: {
-        files: [
-          '**/*.html'
-        ]
-      }
-    },
-    clean: {
-      dist: [
-        'assets/build/app.min.css',
-        'assets/build/app.min.js'
-      ]
-    }
-  });
+    });
 
-  // Load tasks
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-sass');
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-notify');
 
-  // Register tasks
-  grunt.registerTask('default', [
-    'clean',
-    'sass',
-    'uglify'
-  ]);
-  grunt.registerTask('dev', [
-    'watch'
-  ]);
+    // Register tasks
+    grunt.registerTask('default', [
+        'clean',
+        'sass',
+        'uglify'
+    ]);
+    grunt.registerTask('dev', [
+        'watch'
+    ]);
 
 };
